c554e86bf9ffd2e18519b6e86b1b4bab4d03d6d1
Giuseppe Pagano <pagano@digitalidea.eu>
2014-09-09 12:25:24 +0200

    color adjustments

--- authlog-parser.rb
+++ authlog-parser.rb
@@ -29,10 +29,10 @@
 
 # Opening String class to add colors for console output
 class String
-def red;            "\033[31m#{self}\033[0m" end
-def green;          "\033[32m#{self}\033[0m" end
-def cyan;           "\033[36m#{self}\033[0m" end
-def bold;           "\033[1m#{self}\033[22m" end
+	def red;            "\033[31m#{self}\033[0m" end
+	def green;          "\033[32m#{self}\033[0m" end
+	def cyan;           "\033[36m#{self}\033[0m" end
+	def bold;           "\033[1m#{self}\033[22m" end
 end
 
 require 'optparse' # OptionParse class is required to parse options
@@ -99,6 +99,15 @@ rescue
 	puts "unknown option!".red
 	puts parser.help
 	exit
+end
+
+if options[:fileOut] != nil
+	class String
+		def red;            self end
+		def green;          self end
+		def cyan;           self end
+		def bold;           self end
+	end
 end
 
 fileIn = ARGV[0]
